23ed0ab04f615206cd4438074ce82f98cf5a8035
santiago <b3ny0077@gmail.com>
2026-05-15 22:07:21 -0400

    v2

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,11 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the jobs screen', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, v2');
+    expect(compiled.querySelector('.app-name')?.textContent).toContain('JBO');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Jobs');
   });
 });
